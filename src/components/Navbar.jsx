d15b2e4fabe38d48cad7b2b138777574768a254d
Vivek K J <vivekkj2004@gmail.com>
2022-11-07 14:36:52 +0530

    Create Events page and add sample events

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import tekwizaLogo from "/public/images/tekwiza-logo-1.png";
-import scetLogo from "/public/images/scet-logo.png";
 
 const Navbar = () => {
   return (
-    <div className="Navbar text-white px-7 flex h-12 w-screen justify-between items-center bg-gray-900 bg-opacity-60">
+    <div className="Navbar text-white px-7 flex h-12 w-screen justify-between items-center  bg-opacity-60">
       <div className="LogoContainer flex">
+        <Link href={'/'}>
         <Image className="h-4 w-auto" src={tekwizaLogo} />
-        <Image
-          className="mx-10 brightness-0 invert h-4 w-auto"
-          src={scetLogo}
-        />
+        </Link>
       </div>
       <div className="Links flex">
-        <h3 className="ml-5 cursor-pointer">Events</h3>
+        <Link href="/events">
+          <h3 className="ml-5 cursor-pointer">Events</h3>
+        </Link>
         <h3 className="ml-5 cursor-pointer">Workshops</h3>
         <h3 className="ml-5 cursor-pointer">Speakers</h3>
         <h3 className="ml-5 cursor-pointer">Contact Us</h3>
