26e2b9237c3bfeb974eb0a8d80eec674b39e76f7
unknown <joeljaison394@gmail.com>
2022-11-10 10:39:22 +0530

    animation were added to the file

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { motion } from 'framer-motion';
 
 const PageHeader = ({ title }) => {
   return (
     <div className="h-30 py-2 lg:py-6 border-gray-900 border-2 text-2xl lg:text-4xl items-center px-4 lg:px-20">
       {title.split(" ").map((t) => (
         <>
+        <motion.div initial="hidden" animate="visible" variants={{
+        hidden: {
+          scale: .8,
+          opacity: 0
+        },
+        visible: {
+          scale: 1,
+          opacity: 1,
+            transition: {
+            delay: .1
+          }
+        },
+        }}>
           <h2 key={t}>{t}</h2>
+        </motion.div>
+          
         </>
       ))}
     </div>
@@ -13,3 +29,4 @@ const PageHeader = ({ title }) => {
 };
 
 export default PageHeader;
+
